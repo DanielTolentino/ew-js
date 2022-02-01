fbd556624749e01995f45eb6bbe7eb8045b8009d
DanielTolentino <danieltolentino@protonmail.com>
2022-02-01 10:30:47 -0300

    add first demo file

---
+++ capitulo01/demo01-variaveis.js
@@ -0,0 +1,12 @@
+let nome = 'Super Man'
+let idade = 100
+let superPoder = 'Super Força'
+
+
+nome = 'Batman'
+superPoder = "Super Rico"
+console.log(
+    `Ǹome: ${nome}
+    Idade: ${idade}
+    Poder: ${superPoder}`
+)

935e0377c833f21546e8d96e3a22f9d562d73074
DanielTolentino <danieltolentino@protonmail.com>
2022-02-01 15:16:27 -0300

    comment class

---
+++ capitulo01/demo04-comentarios.js
@@ -0,0 +1,11 @@
+// comentário em uma única linha
+
+/**
+* Esse código é Hackerzão
+* Author: Daniel Tolentino
+* Data: 01/02/2022
+*/
+
+//ignorando a linha abaixo
+//const varialConstante = 1+2+4+4
+const varialConstante = 1 + 4 // comentando na linha

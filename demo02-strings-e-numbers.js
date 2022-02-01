beec9cfacecaf4d76321731dcd1a422c7f9de7ca
DanielTolentino <danieltolentino@protonmail.com>
2022-02-01 10:43:07 -0300

    add string and numbes demo

---
+++ demo02-strings-e-numbers.js
@@ -0,0 +1,26 @@
+let salarioDoAmigo = 1000
+let meuSalario = "2999.14"
+let salarioCorrigido = Number(meuSalario)
+
+console.log(salarioDoAmigo + salarioCorrigido)
+
+console.log(
+    typeof(salarioDoAmigo),
+    salarioDoAmigo
+)
+
+console.log(
+    typeof(meuSalario),
+    meuSalario
+)
+console.log(
+    isNaN(meuSalario)
+)
+
+let minhaString = 'Olá mundo!'
+let minhaOutraString = 'Olá mundo 2!'
+let minhaStringComVariaveis = 
+`${minhaString} - ${minhaOutraString} AE!` //crase permite executar operações lógicas dentro da variável
+
+console.log(minhaStringComVariaveis)
+
